53b6cdd51b9051b39866e36157a394df61c9bb46
mariapujirahayu <mariapujir@gmail.com>
2019-11-01 14:10:14 +0700

    Menambahkan event click untuk memperbarui informasi skor dan click Reset

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
@@ -1,4 +1,46 @@
 package id.ac.polinema.skorbasket.viewmodels;
 
-public class SharedScore {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+
+public class SharedScore extends AndroidViewModel {
+
+    private MediatorLiveData<Integer> scoreHome;
+    private MediatorLiveData<Integer> scoreVisitor;
+    private MediatorLiveData<Boolean> winner;
+
+    public SharedScore(@NonNull Application application) {
+        super(application);
+        scoreHome = new MediatorLiveData<>();
+        scoreVisitor = new MediatorLiveData<>();
+        winner = new MediatorLiveData<>();
+    }
+
+    public void setScoreHome(Integer score){
+        scoreHome.setValue(score);
+    }
+
+    public LiveData<Integer> getScoreHome(){
+        return scoreHome;
+    }
+
+    public void setScoreVisitor(Integer score){
+        scoreVisitor.setValue(score);
+    }
+
+    public LiveData<Integer> getScoreVisitor(){
+        return scoreVisitor;
+    }
+
+    public void setWinner(Boolean result){
+        winner.setValue(result);
+    }
+
+    public LiveData<Boolean> getWinner(){
+        return winner;
+    }
 }
